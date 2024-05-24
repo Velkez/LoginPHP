221d3624fb795400ecee0a76207b57094466d5a0
Daniel José Sánchez <107335602+Dan2001jose@users.noreply.github.com>
2024-05-23 19:21:04 -0500

    Add files via upload

---
+++ Registro/crud/Editarlg.php
@@ -0,0 +1,19 @@
+<?php include_once('./logic/conexion.php'); //conexion a la base de datos 
+$usuario = $_POST['nnombre']; // Guarda el nombre del usuario ingresado en el formulario 
+$pass = $_POST['npassword']; // Guarda el password del usuario ingresado en el formulario 
+$id = $_POST['idusuario']; //obtenemos el id del usuario a eliminar 
+if (empty($usuario) || empty($pass)) { // Si el usuario o la contraseña están vacíos     
+    header("Location: index.html"); // Redirecciona a la página de index.html 
+
+}
+
+$qr = "UPDATE usuarios SET usuario='$usuario', password='$pass' WHERE idusuario='$id'"; //query para actualizar el usuario 
+$result = mysqli_query($conexion, $qr); //ejecutamos el query 
+if ($result) { // Si se ejecutó correctamente el query    
+    echo "Se ha actualizado el usuario con éxito"; //imprimimos un mensaje     
+    echo "<br><a href='contenido.php'>Regresar página principal</a>";
+    //imprimimos un link para regresar a la pagina principal 
+} else {
+    echo "Hubo un error al actualizar el usuario"; //imprimimos un mensaje 
+} 
+// header("Location: contenido.php"); 
---
+++ Registro/validar.php
@@ -0,0 +1,40 @@
+<?php 
+include_once('./logic/conexion.php'); 
+// session_destroy(); // Destruye la sesión por si hay una activa 
+session_start() ; // Inicia la sesión 
+
+$usuario = $_POST['nnombre']; // Guarda el nombre del usuario ingresado en el formulario 
+$pass = $_POST['npassword']; // Guarda el password del usuario ingresado en el formulario 
+ if(empty($usuario) || empty($pass)){ // Si el usuario o la contraseña están vacíos     
+    header("Location: index.php"); // Redirecciona a la página de index.php     
+    exit(); 
+     
+} 
+ 
+     
+ 
+// $sql="select * from usuarios where password='$pass'"; // Selecciona todos los datos de la tabla usuarios donde el password sea igual al password ingresado 
+$query="SELECT * FROM usuarios WHERE usuario='$usuario' AND password='$pass'"; //query para registrar el usuario y el password  
+$result =mysqli_query($conexion,$query); //ejecutamos el query 
+$array = mysqli_fetch_array($result); // Guarda los datos de la consulta en un array 
+if($array['usuario'] == $usuario && $array['password'] == $pass){ // Si el usuario y el password ingresado son iguales a los de la base de datos     
+    session_start(); // Inicia la sesión  
+    $_SESSION['usuario'] = $usuario; // Guarda el nombre del usuario que inició sesión en la variable usuario     
+    header("Location: contenido.php"); // Redirecciona a la página de contenido.php 
+}else{      
+    echo "Usuario o contraseña incorrectos"; //imprimimos un mensaje     
+    echo "<br><a href='index.php'>Regresar al login</a>"; //imprimimos un link para regresar a la pagina principal 
+} 
+ 
+// if($row = mysqli_fetch_array($result)){ 
+//  if($row['password'] ==  $pass){ 
+//      session_start(); 
+//      $_SESSION['usuario'] = $usuario; 
+//      header("Location: contenido.php"); 
+//  }else{ 
+         
+//  } 
+// }else{ 
+//  header("Location: index.html"); 
+//  exit(); 
+// } 
