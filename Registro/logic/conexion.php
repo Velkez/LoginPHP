2f2cf59e8888c52c9057e280aa415eaae22a863b
Juan Delgado <135554318+ByteSling3r@users.noreply.github.com>
2024-05-23 19:51:59 -0500

    db

---
+++ Registro/logic/conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error al conectar a la base de datos: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa a la base de datos";
+
+$conn->close();
+?>
